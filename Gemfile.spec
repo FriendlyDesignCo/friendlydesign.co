ac02ab89410feec3a06f9771d0fe8d99b5989f9f
Stoyan Vasilev <jaytwoeight@gmail.com>
2013-12-11 18:39:08 -0500

    cleaned up source/root folder
    removed ruby, node, and grunt related files

--- Gemfile.spec
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org'
-gem 'sass', '3.2.x'
-gem 'compass', '0.12.x'
--- sync.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-upload_location='laserjn@50.87.144.35:/home3/laserjn/www/sites/friendly'
-
-scp -P 2222 -r dist $upload_location
